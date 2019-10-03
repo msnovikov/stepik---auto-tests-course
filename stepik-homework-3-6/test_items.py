7c3ad44303eaf2529c9fef4275785983d1ff3677
Mikhail Novikof <9702571@mail.ru>
2019-10-04 00:18:31 +0300

    добавлена фикстура для запуска выбранного браузера с выбранным языком.

--- stepik-homework-3-6/test_items.py
+++ stepik-homework-3-6/test_items.py
@@ -1,2 +1,10 @@
-# TODO написать тест для проверки того, что страница содержит кнопку добавления в корзину
-# страница: http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/
+import time
+
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_the_basket_button_is_on_the_page(browser):
+    browser.get(link)
+    browser.find_element_by_css_selector(".btn-add-to-basket")
+# TODO доделать assert и отправить на ревью
--- stepik_module_2/lesson2_2_step7.py
+++
@@ -1,6 +0,0 @@
-import os
-
-current_dir = os.path.abspath(os.path.dirname(__file__))
-# ile_path = os.path.join(current_dir, 'file.txt')
-print(current_dir)
-print(os.path.abspath(__file__))
