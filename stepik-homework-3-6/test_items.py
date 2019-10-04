f7d0fae44e5d588789dd4d8e58de580cff9e9696
Mokhail <9702571@mail.ru>
2019-10-04 12:29:41 +0300

    add implicitly wait in test_items

--- stepik-homework-3-6/test_items.py
+++ stepik-homework-3-6/test_items.py
@@ -2,6 +2,7 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
 def test_the_basket_button_is_on_the_page(browser):
+    browser.implicitly_wait(3)
     browser.get(link)
     browser.find_element_by_css_selector(".btn-add-to-basket")
 # TODO доделать assert и отправить на ревью
