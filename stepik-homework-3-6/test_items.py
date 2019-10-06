9586868160e0a3f0b3b25e2ad6634843b56570cd
Mikhail Novikof <9702571@mail.ru>
2019-10-06 15:54:30 +0300

    fix file test_items

--- stepik-homework-3-6/test_items.py
+++ stepik-homework-3-6/test_items.py
@@ -17,4 +17,4 @@ def test_the_basket_button_is_on_the_page(browser):
     # находим кнопку с классом btn-add-to-basket на странице
     button_list = browser.find_elements(By.CLASS_NAME, "btn-add-to-basket")
     # проверяем, что длина списка не равна 0, т.е кнопка найдена
-    assert len(button_list) != 0, f"button not found on page {link}"
+    assert len(button_list) != 0, "button not found on page {}".format(link)
